e7e2c87593bd39eed9aba7ac2607776288e8d22e
Vitaly <hellvesper@gmail.com>
2023-12-15 15:46:51 +0200

    Update system.c
    Yet another case-sensitive include fix

--- components/icm20948/system.c
+++ components/icm20948/system.c
@@ -27,7 +27,7 @@
 #include <stdio.h>
 
 /* InvenSense drivers and utils */
-#include "Invn/Devices/Drivers/Icm20948/Icm20948.h"
+#include "Invn/Devices/Drivers/ICM20948/Icm20948.h"
 #include "Invn/Devices/Drivers/Ak0991x/Ak0991x.h"
 #include "Invn/Devices/SensorTypes.h"
 #include "Invn/Devices/SensorConfig.h"
